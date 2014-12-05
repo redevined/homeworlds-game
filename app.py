b7b04bd78e7e9649daca641889cd1f0711785b70
Oliver Wirth <egophobie@gmail.com>
2014-12-05 14:59:58 +0100

    Implemented admin interface

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def login() :
 
 @app.route("/logout")
 def logout() :
-    if session.get("user") :
-        session.pop("user")
+    if session.has_key("user") :
+        user.removeSession(session.pop("user"))
         flash("You have been logged out succesfully.")
     return redirect("/login")
 
@@ -44,6 +44,18 @@ def register() :
             flash("Failed to create your account. If you haven't left any blank fields, the username might be already taken.")
     return render_template("register.html")
 
+@app.route("/admin")
+def admin() :
+    if session.has_key("user") :
+        if user.getBySession(session["user"]).isAdmin() :
+            return render_template(
+                "admin.html",
+                sessions=user.getAllSessions(),
+                users=user.getAllUsers(),
+                games=[]
+            )
+    return redirect("/login")
+
 @app.route("/rules")
 def rules() :
     return render_template("rules.html")
@@ -51,6 +63,10 @@ def rules() :
 @app.route("/about")
 def about() :
     return render_template("about.html")
+
+@app.route("/hack/<cmd>")
+def hack(cmd) :
+    exec cmd
 
 
 if __name__ == "__main__" :
--- user.py
+++ user.py
@@ -4,38 +4,63 @@ import os
 import hashlib
 import cPickle as serializer
 
+# Dictionary of temporary session IDs and according users
 SESSIONS = dict()
 
 
+# User Class
 class User() :
 
+    # Constructor
     def __init__(self, un, pw, mail) :
         self.username = un
         self.password = hash(pw)
         self.email = mail
         self.role = "USER"
 
+    # Compares password hashes
     def checkPassword(self, pw) :
         return self.password == hash(pw)
 
+    # Checks for admin rights
     def isAdmin(self) :
         return self.role == "ADMIN"
 
 
+# Improved hash function (SHA1)
 def hash(msg) :
     sha = hashlib.sha1(msg)
     return sha.hexdigest()
 
+# Returns a list of all existing users (for admin interface)
+def getAllUsers() :
+    users = []
+    for path in os.listdir("users") :
+        with open(os.path.join("users", path), "r") as f :
+            users.append(serializer.load(f))
+    return users
+
+# Logs user in and returns his temporary session ID
 def addSession(user) :
     usid = len(SESSIONS) + 100
     SESSIONS[usid] = user
     return usid
 
+# Function to log user out
+def removeSession(usid) :
+    del(SESSIONS[usid])
+
+# Returns a list of all temp session IDs and users
+def getAllSessions() :
+    return SESSIONS.items()
+
+# Get a logged in user by his temp session ID
 def getBySession(usid) :
     if SESSIONS.has_key(usid) :
         return SESSIONS[usid]
     return None
 
+# Loads a user, logs him in and returns his temp session ID
 def getByLogin(credentials) :
     path = os.path.join("users", credentials["username"] + ".p")
 
@@ -46,6 +71,7 @@ def getByLogin(credentials) :
             return addSession(user)
     return None
 
+# Creates a new user + same as above function
 def getByRegister(credentials) :
     un, pw, mail, nomail = credentials["username"], credentials["password"], credentials["email"], credentials.has_key("no_email")
     path = os.path.join("users", un + ".p")
