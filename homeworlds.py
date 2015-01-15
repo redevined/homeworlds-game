e5cd620ad17f24c34e7b6976b1206370c7c770b1
Oliver Wirth <egophobie@gmail.com>
2015-01-15 17:22:55 +0100

    Extended game logic

--- homeworlds.py
+++ homeworlds.py
@@ -4,9 +4,72 @@ class Game() :
 
     def __init__(self, player1, player2) :
         self.players = [player1, player2]
+        self.active = 0
         self.universe = Universe()
+        self.aproc = ActionProcessor(self)
 
     def parse(self, pstr) :
+        # Parsing strings
+        # Attack: "player system red target"
+        # Build: "player system green color"
+        # Trade: "player system blue target color"
+        # Move: "player system yellow target system"
+        # Discover (Move): "player system yellow target color size"
+        # Sacrifice: "player system sacrifice target" + target.size*action(target.color)
+        # Cataclysm: "player system cataclysm"
+        # Pass: "player pass"
+
+        proc = pstr.split()
+        player = self.players.index(proc[0])
+        sys = proc[1]
+        action = proc[2]
+
+        if action == "s" :
+            n = aproc.sacrifice(player, sys, proc[3])
+            procs = [[player] + f.split() for f in " ".join(proc[4:]).split(player)][1:]
+            for proc in procs :
+                self.parse(" ".join(proc))
+
+        elif action == "y" and len(proc) > 5 :
+            aproc.discover(player, sys, *proc[3:])
+        elif action == "p" :
+            pass
+        else :
+            {
+                "r" : aproc.attack,
+                "g" : aproc.build,
+                "b" : aproc.trade,
+                "y" : aproc.move,
+                "c" : aproc.cataclysm
+            }[action](player, sys, *proc[3:])
+
+        self.active = (player + 1) % 2
+
+
+class ActionProcessor() :
+
+    def __init__(self, inst) :
+        self.game = inst
+
+    def attack(self, player, sys, target) :
+        pass
+
+    def build(self, player, sys, color) :
+        pass
+
+    def trade(self, player, sys, target, color) :
+        pass
+
+    def move(self, player, sys, target, nsys) :
+        pass
+
+    def discover(self, player, sys, target, color, size) :
+        pass
+
+    def sacrifice(self, player, sys, target) :
+        pass
+
+    def cataclysm(self, player, sys) :
         pass
 
 
